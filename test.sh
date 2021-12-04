3d198870c09b3db22264643f5acb0016584d15ae
Chris Pressey <cpressey@catseye.tc>
2021-12-04 16:18:54 +0000

    Run tests w/ Python 2, or 3, or both, depending on what's installed.

--- test.sh
+++ test.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-python2 script/SAWBONES -t || exit 1
-python3 script/SAWBONES -t || exit 1
+APPLIANCES=""
+if command -v python2 > /dev/null 2>&1; then
+    APPLIANCES="python2 $APPLIANCES"
+    python2 script/SAWBONES -t || exit 1
+fi
+if command -v python3 > /dev/null 2>&1; then
+    APPLIANCES="python3 $APPLIANCES"
+    python3 script/SAWBONES -t || exit 1
+fi
+
+if [ "x$APPLIANCES" = "x" ]; then
+    echo "No suitable Python versions found."
+    exit 1
+fi
